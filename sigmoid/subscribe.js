d1d356d95f1d8f2f2c84dab894a2406b09a616a6
Simon <simon.vincente.zimmermann@gmail.com>
2020-01-29 22:08:30 +0100

    removing some code duplication, fixing fb link

--- sigmoid/subscribe.js
+++
@@ -1,4 +0,0 @@
-function subscribe() {
-        require(["mojo/signup-forms/Loader"], 
-        function(L) { L.start({"baseUrl":"mc.us12.list-manage.com","uuid":"898dfcb7ad3a2ae5731a2cbd5","lid":"4f2cf320f4"}) })
-    }; 
