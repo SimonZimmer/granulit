83f3cb8bf9ff6d04d8c2b5ae40a70973c94e2247
Simon <simon.vincente.zimmermann@gmail.com>
2020-10-13 22:54:01 +0200

    adding basic pages and navigation

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('header.html')
 
+@app.route('/audio')
+def audio():
+    return render_template('audio.html')
+
+@app.route('/video')
+def video():
+    return render_template('video.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/bio')
+def bio():
+    return render_template('bio.html')
