5e9f4d3000e590428b167520f6f52a74b0c5a7b5
Simon <simon.vincente.zimmermann@gmail.com>
2020-12-03 21:13:38 +0100

    adding web form skeleton for user login

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
 import os
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -27,3 +28,13 @@ def bio():
 @app.route('/impressum')
 def impressum():
     return render_template('impressum.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/')
+    return render_template('login.html', title='Sign In', form=form)
+
