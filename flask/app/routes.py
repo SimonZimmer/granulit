0ace27a9f236f328a951e33c8129a9d23edc0f3e
Simon <simon.vincente.zimmermann@gmail.com>
2020-12-05 11:19:27 +0100

    adding user login validation for cms skeleton page

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,7 +1,9 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user, login_required
 import os
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/index')
@@ -32,10 +34,25 @@ def impressum():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('cms'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/cms')
+@login_required
+def cms():
+    return render_template('cms.html')
 
